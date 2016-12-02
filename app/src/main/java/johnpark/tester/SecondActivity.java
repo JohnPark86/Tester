2e7f5c031068471a8a8d3b5f4cf2ea04da58de9e
John <jduncanpark@gmail.com>
2016-12-02 08:57:36 -0500

    Move between activities

--- app/src/main/java/johnpark/tester/SecondActivity.java
+++ app/src/main/java/johnpark/tester/SecondActivity.java
@@ -1,24 +1,48 @@
 package johnpark.tester;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 
 public class SecondActivity extends ActionBarActivity {
 
     TextView t;
+    EditText et;
+    Button b;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
+        b = (Button) findViewById(R.id.button4);
         t = (TextView) findViewById(R.id.textView2);
+        et = (EditText) findViewById(R.id.editText);
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getInput();
+            }
+
+
+        });
     }
 
+    private void getInput() {
+        String input = et.getText().toString();
+        Intent in = new Intent(this,MainActivity.class);
+        in.putExtra("input",input);
+        startActivity(in);
+    }
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
