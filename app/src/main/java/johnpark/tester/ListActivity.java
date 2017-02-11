1cd80e5b5c0111907bff8e05297861963bc57cb1
John <jduncanpark@gmail.com>
2017-02-10 21:29:32 -0500

    Implement ListView. To-Do: Check BG colors

--- app/src/main/java/johnpark/tester/ListActivity.java
+++ app/src/main/java/johnpark/tester/ListActivity.java
@@ -1,19 +1,51 @@
 package johnpark.tester;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 
 public class ListActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
 
-        
+        final ArrayList<String> list = new ArrayList<String>();
+        final ListView listview = (ListView) findViewById(R.id.listview);
+
+        String[] values = new String[]{"A", "B", "C", "D", "E", "F", "G"};
+
+        for (int i = 0; i < values.length; ++i) {
+            list.add(values[i]);
+        }
+        final LVAdapter adapter = new LVAdapter(this,
+                android.R.layout.simple_list_item_1, list);
+
+        listview.setAdapter(adapter);
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, final View view,
+                                    int position, long id) {
+                final String item = (String) parent.getItemAtPosition(position);
+                Log.i("item", item);
+            }
+        });
     }
 
     @Override
@@ -37,4 +69,30 @@ public class ListActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    // Private Class
+    private class LVAdapter extends ArrayAdapter<String> {
+
+        HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
+
+        public LVAdapter(Context context, int textViewResourceId, List<String> objects) {
+            super(context, textViewResourceId, objects);
+            for (int i = 0; i < objects.size(); ++i) {
+                mIdMap.put(objects.get(i), i);
+            }
+        }
+
+        @Override
+        public long getItemId(int position) {
+            String item = getItem(position);
+            return mIdMap.get(item);
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return true;
+        }
+
+    }
+
 }
